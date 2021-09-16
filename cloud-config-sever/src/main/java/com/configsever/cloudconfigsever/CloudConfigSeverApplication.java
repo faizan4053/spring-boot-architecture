c0ee1b09ce1bbe632aee5a245d2da7d333fe9dfa
faizan4053 <mohfaiza3@publicisgroupe.net>
2021-09-16 20:18:21 +0530

    Made changes regarding kubernetes deployment

--- cloud-config-sever/src/main/java/com/configsever/cloudconfigsever/CloudConfigSeverApplication.java
+++ cloud-config-sever/src/main/java/com/configsever/cloudconfigsever/CloudConfigSeverApplication.java
@@ -1,13 +1,15 @@
 package com.configsever.cloudconfigsever;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableConfigServer
+//@EnableAutoConfiguration
 public class CloudConfigSeverApplication {
 
 	public static void main(String[] args) {
